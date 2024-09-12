301ead460c0336a20a4d9f8ca6b9fb3ec04297b8
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2024-09-12 14:05:45 +0200

    chore: remove repository

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Task';
-export * from './TaskMap';
-export * from './SlidingTaskMap';
-export * from './TaskQueue';
-export * from './types';
-export * from './error';
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Deletable {
-  destructor: () => void;
-}
-
-export function isDeletable(value: unknown): value is Deletable {
-  return typeof value === 'object' && value !== null && 'destructor' in value && typeof (value as Deletable).destructor === 'function';
-}
